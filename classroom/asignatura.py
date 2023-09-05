5f4d8ebdd3c396a15a39e7caf3bba5d254b755f7
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-09-05 13:36:43 -0500

    Update asignatura.py

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -5,5 +5,5 @@ class Asignatura:
         self._salon = salon
 
     def __str__(self):
-        cadena= self.nombre + " " + self._salon
+        cadena= f"{self.nombre} {self._salon}"
         return cadena
